b04ab8026c544f0b2ef801db5d767e28e306781b
Lucy <boianhlucy1985@gmail.com>
2024-06-12 00:32:59 +1000

    slogan

--- src/Main.js
+++ src/Main.js
@@ -10,6 +10,7 @@ const Main = () =>{
         <video src={video} autoPlay loop muted></video> 
         <div className="content">
             <h1>An International Freight Forwarding Company </h1>
+            <h2>"Your Business, Our Commitment"</h2>
         </div>
     </div>
 
